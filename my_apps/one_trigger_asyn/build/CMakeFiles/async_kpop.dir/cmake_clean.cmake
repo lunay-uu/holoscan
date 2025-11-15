f2251d311c708232a86e5a4f549f689996803d91
lunay-uu <lunayu2002@gmail.com>
2025-11-15 09:33:01 +0100

    update something

---
+++ my_apps/one_trigger_asyn/build/CMakeFiles/async_kpop.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/async_kpop.dir/async_kpop.cpp.o"
+  "CMakeFiles/async_kpop.dir/async_kpop.cpp.o.d"
+  "async_kpop"
+  "async_kpop.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/async_kpop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
