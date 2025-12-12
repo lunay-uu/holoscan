c81958e4a378c4c9aee2c74f2fe3ac416d7a5844
lunay-uu <lunayu2002@gmail.com>
2025-12-12 08:49:14 +0100

    update

---
+++ my_apps/overall/build/CMakeFiles/overall.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/overall.dir/overall.cpp.o"
+  "CMakeFiles/overall.dir/overall.cpp.o.d"
+  "overall"
+  "overall.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/overall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
