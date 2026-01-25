fb4bf4e7f4808593bd0b2b6ffe6f45fda75f6f21
lunay-uu <lunayu2002@gmail.com>
2026-01-25 16:16:52 +0100

    Add src and experiments folders

--- my_apps/overall/overall.cpp
+++ my_apps/overall/overall.cpp
@@ -12,7 +12,12 @@ size_t tok_op1_op3 = 0;
 size_t tok_op2_op3 = 0;  
 size_t tok_op3_op4 = 0; 
 }
-
+void busy_loop(uint64_t iters) {
+    volatile uint64_t x = 0;
+    for (uint64_t i = 0; i < iters; ++i) {
+      x += i;
+    }
+  }
 
 namespace holoscan::conditions {
 
@@ -192,7 +197,13 @@ class Op1 : public holoscan::Operator {
   void compute(holoscan::InputContext&, holoscan::OutputContext& out,
                holoscan::ExecutionContext&) override {
     static int counter1 = 0;
+
     static int counter2 = 0;
+  int cpu = sched_getcpu();
+  pid_t tid = syscall(SYS_gettid);
+
+//  std::cout << "[Op1] running on CPU " << cpu
+  //          << " (tid=" << tid << ")" << std::endl;
     std::vector<int> b1;
     b1.reserve(a1_.get());
     for (int i = 0; i < a1_.get(); ++i)
@@ -203,6 +214,7 @@ class Op1 : public holoscan::Operator {
     for (int i = 0; i < a2_.get(); ++i)
       b2.push_back(++counter2);
 
+  busy_loop(5e7);
     out.emit(b1, "out1");
     out.emit(b2, "out2");
 
@@ -211,7 +223,7 @@ class Op1 : public holoscan::Operator {
 //std::cout << holoscan::conditions::tok_op1_op3 << std::endl;
     std::cout << "Op1 produces out1:";
     for (auto v : b1) std::cout << " " << v;
-     std::cout <<  std::endl;
+//     std::cout <<  std::endl;
       std::cout << "Op1 produces out2:";
     for (auto v : b2) std::cout << " " << v;
      std::cout <<  std::endl;
@@ -229,17 +241,22 @@ class Op2 : public holoscan::Operator {
   Op2() = default;
 
   void setup(holoscan::OperatorSpec& spec) override {
-    spec.input<std::vector<int>>("in").queue_size(64);
+    spec.input<std::vector<int>>("in").queue_size(2);
     spec.output<std::vector<int>>("out");
 
     spec.param(b1_, "b1", "Consume rate", "Tokens consumed per fire", 3);
-    spec.param(a3_, "a3", "Produce rate", "Tokens produced per fire", 2);
+    spec.param(a3_, "a3", "Produce rate", "Tokens produced per fire", 3);
   }
 
   void compute(holoscan::InputContext& in, holoscan::OutputContext& out,
                holoscan::ExecutionContext&) override {
     static std::vector<int> buffer;
  static int counter = 0;
+  int cpu = sched_getcpu();
+  pid_t tid = syscall(SYS_gettid);
+
+ // std::cout << "[Op2] running on CPU " << cpu
+  //          << " (tid=" << tid << ")" << std::endl;
     auto msg = in.receive<std::vector<int>>("in");
     while (msg) {
       buffer.insert(buffer.end(), msg->begin(), msg->end());
@@ -249,7 +266,7 @@ class Op2 : public holoscan::Operator {
     if (buffer.size() >= static_cast<size_t>(b1_.get())) {
        std::cout << "Op2 consumes:";
       for (int i = 0; i < b1_.get(); ++i) std::cout << " " << buffer[i];
-      std::cout << std::endl;
+  //    std::cout << std::endl;
       buffer.erase(buffer.begin(), buffer.begin() + b1_.get());
       holoscan::conditions::tok_op1_op2 -= b1_.get();
 
@@ -258,12 +275,19 @@ class Op2 : public holoscan::Operator {
       for (int i = 0; i < a3_.get(); ++i)
         produced.push_back(++counter);
 
-      out.emit(produced, "out");
-      holoscan::conditions::tok_op2_op3 += produced.size();
+     
+
+
+      out.emit(produced, "out");    
+  holoscan::conditions::tok_op2_op3 += produced.size();
 //std::cout << holoscan::conditions::tok_op2_op3 << std::endl;
           std::cout << "Op2 produces :";
     for (auto v : produced) std::cout << " " << v;
      std::cout <<  std::endl;
+
+ busy_loop(5e7);
+
+
     }
   }
 
@@ -279,20 +303,25 @@ class Op3 : public holoscan::Operator {
   Op3() = default;
 
   void setup(holoscan::OperatorSpec& spec) override {
-    spec.input<std::vector<int>>("in2").queue_size(64);
-    spec.input<std::vector<int>>("in3").queue_size(64);
+    spec.input<std::vector<int>>("in2").queue_size(4);
+    spec.input<std::vector<int>>("in3").queue_size(2);
     spec.output<std::vector<int>>("out");
 
     spec.param(b2_, "b2", "Consume in2", "Tokens consumed from in2", 2);
-    spec.param(b3_, "b3", "Consume in3", "Tokens consumed from in3", 3);
-    spec.param(a4_, "a4", "Produce out", "Tokens produced per fire", 3);
+    spec.param(b3_, "b3", "Consume in3", "Tokens consumed from in3", 4);
+    spec.param(a4_, "a4", "Produce out", "Tokens produced per fire", 1);
   }
 
   void compute(holoscan::InputContext& in, holoscan::OutputContext& out,
                holoscan::ExecutionContext&) override {
     static std::vector<int> buf2, buf3;
     static int counter = 0;
-    auto m2 = in.receive<std::vector<int>>("in2");
+  int cpu = sched_getcpu();
+  pid_t tid = syscall(SYS_gettid);
+
+//  std::cout << "[Op3] running on CPU " << cpu
+//            << " (tid=" << tid << ")" << std::endl;   
+ auto m2 = in.receive<std::vector<int>>("in2");
     while (m2) {
       buf2.insert(buf2.end(), m2->begin(), m2->end());
       m2 = in.receive<std::vector<int>>("in2");
@@ -308,10 +337,10 @@ class Op3 : public holoscan::Operator {
         buf3.size() >= static_cast<size_t>(b3_.get())) {
 std::cout << "Op3 consumes in1:";
 for (int i = 0; i <  b2_.get(); ++i) std::cout << " " << buf2[i];
-std::cout << std::endl;
+//std::cout << std::endl;
 std::cout << "Op3 consumes in2:";
 for (int i = 0; i <  b3_.get(); ++i) std::cout << " " << buf3[i];
-std::cout << std::endl;
+//std::cout << std::endl;
       buf2.erase(buf2.begin(), buf2.begin() + b2_.get());
       buf3.erase(buf3.begin(), buf3.begin() + b3_.get());
 
@@ -329,6 +358,7 @@ std::cout << std::endl;
               std::cout << "Op3 produces :";
     for (auto v : produced) std::cout << " " << v;
      std::cout <<  std::endl;
+ busy_loop(5e7);
     }
   }
 
@@ -344,13 +374,18 @@ class Op4 : public holoscan::Operator {
   Op4() = default;
 
   void setup(holoscan::OperatorSpec& spec) override {
-    spec.input<std::vector<int>>("in").queue_size(64);
-    spec.param(b4_, "b4", "Consume rate", "Tokens consumed per fire", 4);
+    spec.input<std::vector<int>>("in").queue_size(2);
+    spec.param(b4_, "b4", "Consume rate", "Tokens consumed per fire", 2);
   }
 
   void compute(holoscan::InputContext& in, holoscan::OutputContext&,
                holoscan::ExecutionContext&) override {
     static std::vector<int> buffer;
+  int cpu = sched_getcpu();
+  pid_t tid = syscall(SYS_gettid);
+
+ // std::cout << "[Op4] running on CPU " << cpu
+   //         << " (tid=" << tid << ")" << std::endl;
 
     auto msg = in.receive<std::vector<int>>("in");
     while (msg) {
@@ -364,7 +399,7 @@ for (int i = 0; i <  b4_.get(); ++i) std::cout << " " << buffer[i];
 std::cout << std::endl;
       buffer.erase(buffer.begin(), buffer.begin() + b4_.get());
       holoscan::conditions::tok_op3_op4 -= b4_.get();
-  
+   busy_loop(5e7);
     }
   }
 
@@ -381,7 +416,7 @@ class TokenSDFApp : public holoscan::Application {
 
     auto op1 = make_operator<Op1>(
       "op1",
-      make_condition<CountCondition>("count", 20),
+      make_condition<CountCondition>("count", 96),
       Arg("a1", 2),   // Op1 → Op2 : a1 = 2
       Arg("a2", 1)    // Op1 → Op3 : a2 = 1
     );
@@ -397,7 +432,7 @@ class TokenSDFApp : public holoscan::Application {
       "op2",
       cond2,
       Arg("b1", 3),   // consume 3
-      Arg("a3", 2)    // produce 2
+      Arg("a3", 3)    // produce 2
     );
 
   
@@ -406,7 +441,7 @@ class TokenSDFApp : public holoscan::Application {
       Arg("receiver1", "in2"),
       Arg("receiver2", "in3"),
       Arg("min1", uint64_t(2)),   // Op1 → Op3 consume
-      Arg("min2", uint64_t(3))    // Op2 → Op3 consume
+      Arg("min2", uint64_t(4))    // Op2 → Op3 consume
     );
     auto cond3_in2 = make_condition<conditions::BufferAwareCondition>(
       "cond3_in2",
@@ -420,7 +455,7 @@ class TokenSDFApp : public holoscan::Application {
       "cond3_in3",
       Arg("receiver", "in3"),
       Arg("edge_id", 3),                
-      Arg("min_tokens", uint64_t(3))    
+      Arg("min_tokens", uint64_t(4))    
     );
 
     auto op3 = make_operator<Op3>(
@@ -428,8 +463,8 @@ class TokenSDFApp : public holoscan::Application {
       cond3_in2,
       cond3_in3,
       Arg("b2", 2),
-      Arg("b3", 3),
-      Arg("a4", 3)
+      Arg("b3", 4),
+      Arg("a4", 1)
     );
   //  auto op3 = make_operator<Op3>(
     //  "op3",
@@ -444,13 +479,13 @@ class TokenSDFApp : public holoscan::Application {
       "cond4",
       Arg("receiver", "in"),
       Arg("edge_id", 4),          // tok_op3_op4
-      Arg("min_tokens", uint64_t(4))  // b4 = 4
+      Arg("min_tokens", uint64_t(2))  // b4 = 4
     );
 
     auto op4 = make_operator<Op4>(
       "op4",
       cond4,
-      Arg("b4", 4)
+      Arg("b4", 2)
     );
 
 
@@ -458,18 +493,38 @@ class TokenSDFApp : public holoscan::Application {
     add_flow(op1, op3, {{"out2", "in2"}});
     add_flow(op2, op3, {{"out",  "in3"}});
     add_flow(op3, op4, {{"out",  "in"}});
-    this->scheduler(
-        this->make_scheduler<holoscan::EventBasedScheduler>(
-            "event-scheduler",
-            holoscan::Arg("worker_thread_number", int64_t(1))
-        )); 
+  //  this->scheduler( this->make_scheduler<holoscan::EventBasedScheduler>( "event-scheduler", holoscan::Arg("worker_thread_number", int64_t(4)) )); 
+
+
+//app->scheduler(
+ // app->make_scheduler<holoscan::GreedyScheduler>("greedy")
+//);
+
+
+
+//   app->scheduler(app->make_scheduler<holoscan::MultiThreadScheduler>("mts", holoscan::Arg("worker_thread_number", int64_t(4))  ));
+
+
  }
 };
 
 
 
 int main() {
-  auto app = holoscan::make_application<TokenSDFApp>();
+  
+auto app = holoscan::make_application<TokenSDFApp>();
+
+
+//    app->scheduler( app->make_scheduler<holoscan::EventBasedScheduler>( "event-scheduler", holoscan::Arg("worker_thread_number", int64_t(4)) ));
+
+//app->scheduler( app->make_scheduler<holoscan::GreedyScheduler>("greedy"));
+
+
+
+   app->scheduler(app->make_scheduler<holoscan::MultiThreadScheduler>("mts", holoscan::Arg("worker_thread_number", int64_t(4))  ));
+
+
+
   app->run();
   return 0;
 }
