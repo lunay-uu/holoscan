ff9c25fd4ab1e9e6bff6d53e4992a706fc7ee53d
lunay-uu <lunayu2002@gmail.com>
2026-02-25 15:16:24 +0800

    Update system_loop.cpp

--- my_apps/system_loop/system_loop.cpp
+++ my_apps/system_loop/system_loop.cpp
@@ -182,9 +182,9 @@ class Op2 : public holoscan::Operator {
       buffer.insert(buffer.end(), msg->begin(), msg->end());
       msg = in.receive<std::vector<int>>("in");
     }
-
-    if (buffer.size() >= static_cast<size_t>(b1_.get())) {
        std::cout << "[Op2] consumes:";
+    if (buffer.size() >= static_cast<size_t>(b1_.get())) {
+
       for (int i = 0; i < b1_.get(); ++i) std::cout << " " << buffer[i];
   //    std::cout << std::endl;
       buffer.erase(buffer.begin(), buffer.begin() + b1_.get());
@@ -252,10 +252,10 @@ class Op3 : public holoscan::Operator {
       buf3.insert(buf3.end(), m3->begin(), m3->end());
       m3 = in.receive<std::vector<int>>("in3");
     }
-
+  std::cout << "[Op3] consumes [in1]:";
     if (buf2.size() >= static_cast<size_t>(b2_.get()) &&
         buf3.size() >= static_cast<size_t>(b3_.get())) {
-std::cout << "[Op3] consumes [in1]:";
+
 for (int i = 0; i <  b2_.get(); ++i) std::cout << " " << buf2[i];
 //std::cout << std::endl;
 std::cout << "[Op3] consumes [in2]:";
@@ -312,9 +312,10 @@ class Op4 : public holoscan::Operator {
       buffer.insert(buffer.end(), msg->begin(), msg->end());
       msg = in.receive<std::vector<int>>("in");
     }
-
-    //if (buffer.size() >= static_cast<size_t>(b4_.get())) {
       std::cout << "[Op4] consumes:";
+
+    if (buffer.size() >= static_cast<size_t>(b4_.get())) {
+
       for (int i = 0; i <  b4_.get(); ++i) std::cout << " " << buffer[i];
       std::cout << std::endl;
       buffer.erase(buffer.begin(), buffer.begin() + b4_.get());
@@ -324,7 +325,7 @@ class Op4 : public holoscan::Operator {
       tok_op4_op1+=1;
     }
    busy_loop(5e7);
-    //}
+    }
   }
 
  private:
